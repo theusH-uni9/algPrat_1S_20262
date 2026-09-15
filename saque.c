950e0e5c8c833dc8cb160927869cd2ab9760eb7d
theusH-uni9 <matheushbmarques16@uni9.edu.br>
2026-09-15 15:23:18 +0000

    operadores e condicoes

--- saque.c
+++ saque.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    int saque, n50 = 0, n20 = 0, n10 = 0;
+int main() {
+
+     /*int saque, n50 = 0, n20 = 0, n10 = 0;
     
     printf("Por favor, digite o valor para o saque em múltiplos de 10: ");
     scanf("%d",&saque);
-   
-// if(saque % 10 != 0) {print e return 1}
 
    if(saque % 10 == 0){ 
         n50 = saque / 50; // Calcula quantas notas de 50 são possiveis
@@ -21,7 +19,37 @@ int main()
         printf("-- Saque efetuado --\nNotas de R$50: %d\nNotas de R$20: %d\nNotas de R$10: %d\n",n50,n20,n10);
    } else {
         printf("Valor inválido! Por favor, digite um valor múltiplo de 10.\n");
-   }
+   }*/
 
-    return 0;
+     int cedulas = 0, saque, saldo = 0;
+
+     printf("Digite o valor que deseja sacar: R$");
+     scanf("%d",&saque);
+
+     if (saque % 10 != 0){
+          printf("Impossível sacar esse valor: R$%d\n", saque);
+          return 1;
+     }
+
+     cedulas = saque / 50;
+     saldo = saque % 50;
+
+     if (cedulas){
+          printf("Cedulas de R$50: %d\n", cedulas);
+     }
+
+     cedulas = saldo / 20;
+     saldo %= 20;
+
+     if (cedulas){
+          printf("Cedulas de R$20: %d\n", cedulas);
+     }
+
+     cedulas = saldo / 10;
+
+     if (cedulas){
+          printf("Cedulas de R$10: %d\n", cedulas);
+     }
+
+     return 0;
 }
